a7d685c06ba8adecc4fa0deb7b6b12f232409d63
Artūrs Vīksne <artursviksne04@gmail.com>
2021-02-20 11:10:19 +0200

    Update 10.php

--- Php_basics/Arithmetic_operations/10.php
+++ Php_basics/Arithmetic_operations/10.php
@@ -5,30 +5,43 @@ class Geometry
     public float $num1;
     public float $num2;
 
+    public function testPositive(float $num1, float $num2 = null): void {
+        if ($num1 < 0 || $num2 < 0) {
+            throw new Error('Only positive values!');
+        }
+    }
+
     public function circleArea(): string
     {
-
+        $this->setNum1(readline('Radius: '));
+        $this->testPositive($this->num1);
         return 'Area: ' . M_PI * $this->num1 ** 2;
     }
 
     public function rectangleArea(): string
     {
+        $this->setNum1(readline('Length: '));
+        $this->setNum2(readline('Width: '));
+        $this->testPositive($this->num1, $this->num2);
         return 'Area: ' . $this->num1 * $this->num2;
     }
 
     public function triangleArea(): string
     {
+        $this->setNum1(readline('Length: '));
+        $this->setNum2(readline('Width: '));
+        $this->testPositive($this->num1, $this->num2);
         return 'Area: ' . $this->num1 * $this->num2 * 0.5;
     }
 
-    public function setNum1(int $num1): void
+    public function setNum1(string $num1): void
     {
-        $this->num1 = $num1;
+        $this->num1 = (float)$num1;
     }
 
-    public function setNum2(int $num2): void
+    public function setNum2(string $num2): void
     {
-        $this->num2 = $num2;
+        $this->num2 = (float)$num2;
     }
 
 }
@@ -39,32 +52,18 @@ echo "Geometry Calculator:" . PHP_EOL
     . "3. Calculate the Area of a Triangle" . PHP_EOL
     . "4. Quit" . PHP_EOL;
 
-function testPositive(float $num1, float $num2 = null): void {
-    if ($num1 < 0 || $num2 < 0) {
-        throw new Error('Only positive values!');
-    }
-}
-
 function calculateGeometry(string $selection): string
 {
     $geometry = new Geometry();
     if ($selection === '1') {
-        $geometry->setNum1(readline('Radius: '));
-        testPositive($geometry->num1);
         return $geometry->circleArea();
     }
 
     if ($selection === '2') {
-        $geometry->setNum1(readline('Length: '));
-        $geometry->setNum2(readline('Width: '));
-        testPositive($geometry->num1, $geometry->num2);
         return $geometry->rectangleArea();
     }
 
     if ($selection === '3') {
-        $geometry->setNum1(readline('Length: '));
-        $geometry->setNum2(readline('Width: '));
-        testPositive($geometry->num1, $geometry->num2);
         return $geometry->triangleArea();
     }
 
