f9c7bbafd77aa4636bee295843e0abb17424cdd9
Artūrs Vīksne <artursviksne04@gmail.com>
2021-02-20 11:15:34 +0200

    Update 01.php

--- Php_basics/Arithmetic_operations/01.php
+++ Php_basics/Arithmetic_operations/01.php
@@ -3,5 +3,4 @@
 $int1 = readline('Integer 1: ');
 $int2 = readline('Integer 2: ');
 
-echo $int1 == 15 || $int2 == 15 || $int1 + $int2 === 15 || abs($int2 - $int1) === 15 ? 'true' : 'false';
-// kaut kādu iemeslu dēļ ar === neiet
+echo $int1 === '15' || $int2 === '15' || $int1 + $int2 === 15 || abs($int2 - $int1) === 15 ? 'true' : 'false';
