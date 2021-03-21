76c8f3ab9864b54cf635bf0173a0a807e9786aeb
Artūrs Vīksne <artursviksne04@gmail.com>
2021-03-21 11:00:29 +0200

    Routes refractor

--- Tasks/MySQL-Flowershop/index.php
+++ Tasks/MySQL-Flowershop/index.php
@@ -1,8 +1,5 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'app/routes.php';
 
-$handler = $routeInfo[1];
-[$class, $method] = $handler;
-(new $class)->$method();
+App::load(require 'app/routes.php');
